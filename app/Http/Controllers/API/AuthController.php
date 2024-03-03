d957dbfd4f75a5e93c230720281502d159eb41a3
Abdul Ghani Raheem <digitalabdulis@gmail.com>
2024-03-04 01:39:18 +0530

    Added Sanctum Token + API namespace updated

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -16,7 +17,7 @@ class AuthController extends Controller
             'name' => 'required|string',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
-            'c_password' => 'required|string|min:6|same',
+            'c_password' => 'required|string|min:6|same:password',
         ]);
 
         if ($validator->fails()) {
@@ -40,6 +41,28 @@ class AuthController extends Controller
             'message' => 'User register successfully',
         ];
         return response()->json($response, 200);
+    }
 
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            /** @var \App\Models\User $user **/
+            $user = Auth::user();
+            $success['token'] = $user->createToken('MyApp')->plainTextToken;            
+            $success['name'] = $user->name;
+
+            $response = [
+                'success' => true,
+                'data' => $success,
+                'message' => 'User login successfully',
+            ];
+            return response()->json($response, 200);
+        }else{
+            $response = [
+                'success' => false,
+                'message' => 'Unauthorized',
+            ];
+            return response()->json($response, 404);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageLayoutController;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\PageLayoutController;
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
 });
 
-Route::apiResource('/page-layouts', PageLayoutController::class);
+Route::group([
+    'middleware' => ['auth:sanctum'],
+], function ($router) {
+    Route::get('user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::apiResource('page-layouts', PageLayoutController::class);
+});
