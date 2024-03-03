4b88c78dfa43a4ab1894b50335dc5074408bb59e
Abdul <abdul.raheem@so-use.de>
2024-03-03 15:03:42 +0530

    Api Auth Controller Update

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        // Validation
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+            'c_password' => 'required|string|min:6|same',
+        ]);
+
+        if ($validator->fails()) {
+            $response = [
+                'success' => false,
+                'message' => $validator->errors(),
+            ];
+            return response()->json($response, 400);
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+
+        $success['token'] = $user->createToken('MyApp')->plainTextToken;
+        $success['name'] = $user->name;
+
+        $response = [
+            'success' => true,
+            'data' => $success,
+            'message' => 'User register successfully',
+        ];
+        return response()->json($response, 200);
+
+    }
+}
